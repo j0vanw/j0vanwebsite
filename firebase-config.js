b219506d90d7270a14a096d59eabbfe13e73ebbd
j0vanw <skybrojw@gmail.com>
2025-05-16 19:29:05 +0800

    Updated 3 files regarding firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,6 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-export { auth };
+export default app;
